271489d9eb48346cd99e464e5eca220f7edd5d3f
akjosharora <akjosharora@gmail.com>
2018-08-27 12:36:41 +0100

    NEW: Tracking untracked files
    NEW: Tracking files.
    
    NEW: requirements.txt

---
+++ tweetsent/sentiment/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(''                 , views.tweets          , name='tweets'         ),
+    path('tweets'                 , views.tweets          , name='tweets'         ),
+    path('add_classifier'   , views.add_classifier  , name='add_classifier' ),
+    path('delete_classifier'   , views.delete_classifier  , name='delete_classifier' ),
+    path('classifiers'      , views.classifiers     , name='classifiers'    ),
+    ]
